2475dd21b58c8b798379906e6d98a2e07e342d85
hjson11 <sonjamie11@gmail.com>
2021-06-27 15:28:45 -0400

    First Commit

---
+++ medium.py
@@ -0,0 +1,14 @@
+def shout(phrase):
+    if phrase == phrase.upper():
+        return "YOU'RE SHOUTING!"
+    else:
+        return "Can you speak up?"
+
+shout("HELLO WORLD")
+
+def whisper(phrase):
+    if phrase == phrase.lower():
+        return "you're whispering"
+        
+whisper("hello world")
+
